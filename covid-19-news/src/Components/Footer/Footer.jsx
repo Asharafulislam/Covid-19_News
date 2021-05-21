188f919e4fa8bfc4c999338fe734dc506654ad8c
Asharaful Islam <Asharafulislam1@gmail.com>
2021-05-21 14:24:13 -0400

    Create Footer.jsx

---
+++ covid-19-news/src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export class Footer extends React.Component {
+  render() {
+      return(
+              <div className="copyright"> © 2021 Temp Track Solutions </div>        
+      );
+  }
+}
+export default Footer;
